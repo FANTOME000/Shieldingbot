4516d07111f8540aeec2e918098ce3e3165cebc2
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-02-11 22:56:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function () {
-    console.log("Listening on Port 3000");
+console.log("Listening on Port 3000");
 });
 
 client.on('ready', () => {
