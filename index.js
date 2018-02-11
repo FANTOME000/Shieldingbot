42d15b71165290885308f05537c43c3d8e3b9149
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-02-11 22:58:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function () {
-console.log("Listening on Port 3000");
-});
 
 client.on('ready', () => {
     console.log(`Se connecter comme ${client.user.tag}!`);
+    client.listen(port, "0.0.0.0", function () {
+        console.log("Listening on Port 3000");
+    });
+
 });
 
 client.on('message', msg => {
