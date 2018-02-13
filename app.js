5c7c492400b2165d09321cae7191b01baefefcf3
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-02-13 16:46:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ client.on("ready", () => {
     client.user.setPresence({ game: { name: "*" + "help" + " pour afficher l'aide", type: 0 } });
     client.channels.get("412656091613102080").sendMessage({
         embed: {
-            color: "48C9B0",
+            color: 732550,
             title: "Bot ON,prêt a être utiliser !"
         }
     });
