2d6505084d8f5fc4ed33b142d3b2c7ff030650c3
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-02-16 23:10:38 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -88,7 +88,19 @@ client.on("message", msg => {
         var role = member.guild.roles.find("name", "Visiteurs");
         member.addRole(role);
     });
+    if (!message.guild) return;
 
+    if (message.content === '/join') {
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel.join()
+                .then(connection => { 
+                    message.reply('I have successfully connected to the channel!');
+                })
+                .catch(console.log);
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
 
     // fin du code message
 });
