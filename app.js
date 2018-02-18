0499fd5d56987b7fc1f25bf531da9126646991c5
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-02-18 18:04:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -56,15 +56,19 @@ client.on("message", msg => {
                 fields: [
                     {
                         name: "Chanels",
-                        value: "Il y a actuellement " + "**" + msg.guild.channels.size + "** " + " channels dans ce serveur"
+                        value: "Il y a actuellement " + "**" + msg.guild.channels.size + "** " + "channels dans ce serveur"
                     },
                     {
                         name: "Nombres de Membres dans le Serveur",
-                        value: "Il y a exactement" + "**" + msg.guild.members.size + "** " + " membres dans ce serveur"
+                        value: "Il y a exactement " + "**" + msg.guild.members.size + "** " + "membres dans ce serveur"
                     },
                     {
                         name: "Date de créations du serveur",
                         value: "Le serveur a été crée le: " + "**" + msg.guild.createdAt + "**"
+                    },
+                    {
+                        name: "Nombres de serveur ou es " + client.user.username,
+                        value: "Je suis present dans "  + "**"  + bot.guilds.size +  "**"  + " serveurs"
                     },
                 ],
                 timestamp: new Date(),
