904eb570b3f6c3e083caf41e4de3a3971704bd02
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-02-18 19:21:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
     // log bot bien lancer
     console.log(`Se connecter comme ${client.user.tag}!`);
     // activiter du bot
-    client.user.setGame("\"" + pre + "help\" pour afficher les commandes", 'https://www.twitch.tv/')
+    client.user.setGame("\"" + pre + "help\" pour afficher les commandes", 'https://www.twitch.tv/raid')
     // message de mise en route du bot
     client.channels.get("413027512122081280").sendMessage({
         embed: {
