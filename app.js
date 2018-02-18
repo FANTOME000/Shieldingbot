e80977f83d3ccabe424062744ac19ab4dc8ce443
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-02-18 15:27:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ client.on("message", msg => {
     }
 
     else if (msg.content.startsWith("*warn")) {
-        msg.content.replace("*warn ", "").msg.channel.send("lol mdr");
+        msg.content.reply("*warn ", "");
 
 
     }
